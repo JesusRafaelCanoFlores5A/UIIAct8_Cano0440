3951850ac36742043e33a74860b6fcac2978200c
JESUS_RAFAEL_CANO_FLORES <a.21308051280440@cbtis128.edu.mx>
2024-05-06 17:19:59 +0000

    update 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,141 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+/// Flutter code sample for [TabBar].
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const TabBarApp());
+
+class TabBarApp extends StatelessWidget {
+  const TabBarApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+      theme: ThemeData(useMaterial3: true),
+      home: const TabBarExample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class TabBarExample extends StatefulWidget {
+  const TabBarExample({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TabBarExample> createState() => _TabBarExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+/// [AnimationController]s can be created with `vsync: this` because of
+/// [TickerProviderStateMixin].
+class _TabBarExampleState extends State<TabBarExample>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xff077aff),
         title: const Text(
           'Comex Cano 0440',
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: const Color(0xff0790ff),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const <Widget>[
+            Tab(
+              icon: Icon(
+                Icons.favorite_border_outlined,
+                color: Colors.white,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.sunny,
+                color: Colors.white,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.bed,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Image.asset(
-        "assets/images/Amor.jpg",
-        width: 400,
-        height: 300,
-        fit: BoxFit.contain,
-        frameBuilder: (context, child, frame, loaded) {
-          if (loaded) {
-            return child;
-          }
-          return AnimatedOpacity(
-            opacity: frame == null ? 0 : 1,
-            duration: const Duration(seconds: 1),
-            curve: Curves.easeOut,
-            child: child,
-          );
-        },
-        errorBuilder: (context, exception, stackTrace) {
-          return const Center(
-            child: Text("Algo salió mal, por favor vuelva a intentar"),
-          );
-        },
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Center(
+            child: Image.asset(
+              "assets/images/Amor.jpg",
+              width: 400,
+              height: 300,
+              fit: BoxFit.contain,
+              frameBuilder: (context, child, frame, loaded) {
+                if (loaded) {
+                  return child;
+                }
+                return AnimatedOpacity(
+                  opacity: frame == null ? 0 : 1,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.easeOut,
+                  child: child,
+                );
+              },
+            ),
+          ),
+          Center(
+            child: Image.asset(
+              "assets/images/BrilloRadiante.jpg",
+              width: 400,
+              height: 300,
+              fit: BoxFit.contain,
+              frameBuilder: (context, child, frame, loaded) {
+                if (loaded) {
+                  return child;
+                }
+                return AnimatedOpacity(
+                  opacity: frame == null ? 0 : 1,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.easeOut,
+                  child: child,
+                );
+              },
+            ),
+          ),
+          Center(
+            child: Image.asset(
+              "assets/images/Descanso.jpg",
+              width: 400,
+              height: 300,
+              fit: BoxFit.contain,
+              frameBuilder: (context, child, frame, loaded) {
+                if (loaded) {
+                  return child;
+                }
+                return AnimatedOpacity(
+                  opacity: frame == null ? 0 : 1,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.easeOut,
+                  child: child,
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
